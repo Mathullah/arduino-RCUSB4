82b032adb8530d541fd4d97c8b9411f268663185
Mathias Felix <mathias.felix@maxongroup.com>
2023-02-03 06:37:43 +0100

    Fixture added. Unit test extended.

--- UnitTest/Tests/RcPwmTest.cpp
+++ UnitTest/Tests/RcPwmTest.cpp
@@ -2,10 +2,52 @@
 #include "../../src/RcPwm/RcPwm.hpp"
 #include <gtest/gtest.h>
 
+using namespace testing;
 
-TEST(RcPwmtest, Return_zero_after_creation)
+class RcPwmTest : public ::testing::Test
 {
-    RcPwm PwmInput;
+protected:
+    RcPwm m_UUT;
 
-    ASSERT_EQ(PwmInput.Get(), 0U);
+    void SetUp(void) override
+    {
+    }
+
+    void TearDown(void) override
+    {
+    }
+
+    /**
+     * @brief Helper function to generate a RC pulse with a defined length
+     * @PulseLength Desired pulse length [us]
+     */
+    void GeneratePulse(uint32_t const PulseLength)
+    {
+        uint32_t Time{1291};
+        m_UUT.Isr(uint8_t(1), Time);
+        m_UUT.Isr(uint8_t(0), Time + PulseLength);
+    }
+
+    /**
+     * @brief Helper function to generate a RC pulse given by two timestamps
+     * @RisingEdge Time (stamp) at rising edge [us]
+     * @FallingEdge Time (stamp) at falling edge [us]
+     */
+    void GeneratePulse(uint32_t const RisingEdge, uint32_t const FallingEdge)
+    {
+        m_UUT.Isr(uint8_t(1), RisingEdge);
+        m_UUT.Isr(uint8_t(0), FallingEdge);
+    }
+};
+
+TEST_F(RcPwmTest, Return_zero_after_creation)
+{
+    EXPECT_EQ(m_UUT.Get(), 0U);
+}
+
+TEST_F(RcPwmTest, Return_non_zero_after_creation)
+{
+    GeneratePulse(1500);
+
+    EXPECT_EQ(m_UUT.Get(), 0U);
 }
