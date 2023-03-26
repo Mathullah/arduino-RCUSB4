c223b3b3730f628a5e1ffd91f17c3921bbcdd6c8
Mathullah <54320552+Mathullah@users.noreply.github.com>
2023-03-26 22:27:30 +0200

    Thread safety improvements and comments reworked

--- src/RcPwm/RcPwm.cpp
+++ src/RcPwm/RcPwm.cpp
@@ -1,7 +1,20 @@
+/**
+ * @file RcPwm.cpp
+ * @author Mathias Felix
+ * @brief RC to USB Interface RC PWM class
+ *
+ *        Original version by kekse23.de RCUSB
+ *        Copyright (c) 2019, Nicholas Regitz
+ *
+ *        Diese Datei ist Lizensiert unter der Creative Commons 4.0 CC BY-NC-SA
+ *        https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 
 #include "RcPwm.hpp"
-
-
+#include <util/atomic.h>
 
 void RcPwm::Isr(uint8_t const Input, uint32_t const TimeStamp)
 {
@@ -11,11 +24,12 @@ void RcPwm::Isr(uint8_t const Input, uint32_t const TimeStamp)
     {
         m_PositiveEdge = Time;
     }
-    else if (Time > m_PositiveEdge)  // capture negative edge (at least 1us later)
+    else if (Time > m_PositiveEdge) // capture negative edge
     {
         m_Value = (m_Value + (Time - m_PositiveEdge)) / 2;
         m_NewValueIsAvailable = true;
-    } else
+    }
+    else
     {
         // nothing to do.
     }
@@ -23,11 +37,20 @@ void RcPwm::Isr(uint8_t const Input, uint32_t const TimeStamp)
 
 uint16_t RcPwm::Get()
 {
-    if (m_NewValueIsAvailable)
+    bool IsAvailable{};
+    uint16_t Pulse{};
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
-        auto const LastPulseLength{m_PulseTime};
-        m_PulseTime = static_cast<uint16_t>((LastPulseLength + m_Value) / 2);
+        IsAvailable = m_NewValueIsAvailable;
         m_NewValueIsAvailable = false;
+        Pulse = m_Value;
     }
+
+    if (IsAvailable)
+    {
+        m_PulseTime = static_cast<uint16_t>((m_PulseTime + Pulse) / 2);
+    }
+
     return m_PulseTime;
 }
