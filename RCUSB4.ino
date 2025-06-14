a64a02ff2b6d7eba329094e9a5ed32f44f08096e
Mathullah <54320552+Mathullah@users.noreply.github.com>
2025-06-14 11:22:15 +0200

    Code cleanup, settings for using project specific clang format file Thread safety improvements and comments reworked and Autosend deactivated (better channel coherency)

--- RCUSB4.ino
+++ RCUSB4.ino
@@ -1,64 +1,40 @@
-/*
- * RCUSB Interface
- * Modified version
- * Copyright (c) 2022, Mathias Felix
+/**
+ * @file RCUSB4.ino
+ * @author Mathias Felix
+ * @brief RC to USB Interface main file, modified version
  *
- * kekse23.de RCUSB
- * Original version
- * Copyright (c) 2019, Nicholas Regitz
+ *        Original version by kekse23.de RCUSB
+ *        Copyright (c) 2019, Nicholas Regitz
  *
- * Diese Datei ist Lizensiert unter der Creative Commons 4.0 CC BY-NC-SA
- * https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *        Diese Datei ist Lizensiert unter der Creative Commons 4.0 CC BY-NC-SA
+ *        https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * @copyright Copyright (c) 2023
+ *
  */
 
+#include "src/RcPwm/RcPwm.hpp"
 #include <Joystick.h>
 
-class RcInput
-{
-public:
-    RcInput() :
-        m_PulseTime{},
-        m_PositiveEdge{},
-        m_NegativeEdge{},
-        m_Value{},
-        m_NewValueIsAvailable{}
-    {
-
-    }
-
-    uint16_t    Get();
-    void        Isr(uint8_t const Input, uint32_t const TimeStamp);
-
-protected:
-private:
-    uint16_t         m_PulseTime;
-    volatile int32_t m_PositiveEdge;
-    volatile int32_t m_NegativeEdge;
-    volatile int32_t m_Value;
-    volatile bool    m_NewValueIsAvailable;
-};
-
-
-static constexpr uint8_t InputPinCh_1{1};
-static constexpr uint8_t InputPinCh_2{0};
-static constexpr uint8_t InputPinCh_3{2};
-static constexpr uint8_t InputPinCh_4{3};
+static constexpr uint8_t InputPinCh_1{3};
+static constexpr uint8_t InputPinCh_2{2};
+static constexpr uint8_t InputPinCh_3{0};
+static constexpr uint8_t InputPinCh_4{1};
 
 static constexpr uint16_t AxisUpperBound{2250U};
 static constexpr uint16_t AxisLowerBound{750U};
 
-Joystick_ Joystick( JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
-                    0, 0,                     // Button Count, Hat Switch Count
-                    true, true, false,        // X, Y, Z
-                    true, true, false,        // Rx, Ry, Rz
-                    false, false,             // Rudder, Throttle
-                    false, false, false);     // Accelerator, Brake, Steering
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
+                   0, 0,                 // Button Count, Hat Switch Count
+                   true, true, false,    // X, Y, Z
+                   true, true, false,    // Rx, Ry, Rz
+                   false, false,         // Rudder, Throttle
+                   false, false, false); // Accelerator, Brake, Steering
 
-RcInput RcChannel_X;
-RcInput RcChannel_Y;
-RcInput RcChannel_Rx;
-RcInput RcChannel_Ry;
-
+RcPwm RcChannel_X;
+RcPwm RcChannel_Y;
+RcPwm RcChannel_Rx;
+RcPwm RcChannel_Ry;
 
 void setup()
 {
@@ -67,20 +43,20 @@ void setup()
     pinMode(InputPinCh_3, INPUT_PULLUP);
     pinMode(InputPinCh_4, INPUT_PULLUP);
 
-    Joystick.begin();
+    Joystick.begin(false); // initAutoSendState not set
 
     Joystick.setXAxisRange(AxisUpperBound, AxisLowerBound);
     Joystick.setYAxisRange(AxisUpperBound, AxisLowerBound);
     Joystick.setRxAxisRange(AxisUpperBound, AxisLowerBound);
     Joystick.setRyAxisRange(AxisUpperBound, AxisLowerBound);
 
-    /* Config interrupts with corresponding callback functions (lambdas) */
+    /* clang-format off */
     attachInterrupt(digitalPinToInterrupt(InputPinCh_1), []() {RcChannel_X.Isr(digitalRead(InputPinCh_1), micros());}, CHANGE);
     attachInterrupt(digitalPinToInterrupt(InputPinCh_2), []() {RcChannel_Y.Isr(digitalRead(InputPinCh_2), micros());}, CHANGE);
     attachInterrupt(digitalPinToInterrupt(InputPinCh_3), []() {RcChannel_Rx.Isr(digitalRead(InputPinCh_3), micros());}, CHANGE);
     attachInterrupt(digitalPinToInterrupt(InputPinCh_4), []() {RcChannel_Ry.Isr(digitalRead(InputPinCh_4), micros());}, CHANGE);
+    /* clang-format on */
 }
-
 
 void loop()
 {
@@ -90,35 +66,7 @@ void loop()
     Joystick.setRxAxis(RcChannel_Rx.Get());
     Joystick.setRyAxis(RcChannel_Ry.Get());
 
+    Joystick.sendState();
+
     delay(10);
-
-}
-
-void RcInput::Isr(uint8_t const Input, uint32_t const TimeStamp)
-{
-    auto const Time{static_cast<int32_t>(TimeStamp)};
-
-    if (static_cast<bool>(Input)) // capture positive edge
-    {
-        m_PositiveEdge = Time;
-    }
-    else if (Time > m_PositiveEdge)  // capture negative edge (at least 1us later)
-    {
-        m_Value = (m_Value + (Time - m_PositiveEdge)) / 2;
-        m_NewValueIsAvailable = true;
-    } else
-    {
-        // nothing to do.
-    }
-}
-
-uint16_t RcInput::Get()
-{
-    if (m_NewValueIsAvailable)
-    {
-        auto const LastPulseLength{m_PulseTime};
-        m_PulseTime = static_cast<uint16_t>((LastPulseLength + m_Value) / 2);
-        m_NewValueIsAvailable = false;
-    }
-    return m_PulseTime;
 }
---
+++ src/RcPwm/RcPwm.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file RcPwm.cpp
+ * @author Mathias Felix
+ * @brief RC to USB Interface RC PWM class
+ *
+ *        Original version by kekse23.de RCUSB
+ *        Copyright (c) 2019, Nicholas Regitz
+ *
+ *        Diese Datei ist Lizensiert unter der Creative Commons 4.0 CC BY-NC-SA
+ *        https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "RcPwm.hpp"
+#include <util/atomic.h>
+
+
+RcPwm::RcPwm() :
+    m_PulseTime{},
+    m_PositiveEdge{},
+    m_Value{},
+    m_NewValueIsAvailable{}
+{
+}
+
+void RcPwm::Isr(uint8_t const Input, uint32_t const TimeStamp)
+{
+    auto const Time{static_cast<int32_t>(TimeStamp)};
+
+    if (static_cast<bool>(Input)) // capture positive edge
+    {
+        m_PositiveEdge = Time;
+    } else if (Time > m_PositiveEdge) // capture negative edge
+    {
+        m_Value               = static_cast<int16_t>(Time - m_PositiveEdge);
+        m_NewValueIsAvailable = true;
+    } else
+    {
+        // nothing to do.
+    }
+}
+
+uint16_t RcPwm::Get()
+{
+    bool     IsAvailable{};
+    uint16_t Pulse{};
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        IsAvailable           = m_NewValueIsAvailable;
+        m_NewValueIsAvailable = false;
+        Pulse                 = m_Value;
+    }
+
+    if (IsAvailable)
+    {
+        m_PulseTime = static_cast<uint16_t>((m_PulseTime + Pulse) / 2);
+    }
+
+    return m_PulseTime;
+}
